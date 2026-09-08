2a495d7395634531dcb6fc426621b4631df72230
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-09-08 11:49:36 +0700

    ex03

--- Ex03/Ex03.java
+++ Ex03/Ex03.java
@@ -1,1 +1,50 @@
+import java.util.Scanner;
 
+public class Ex03 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Tạo 2 đối tượng Person
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+        // Nhập thông tin Person 1
+        System.out.println("=== NHẬP THÔNG TIN NGƯỜI THỨ NHẤT ===");
+
+        System.out.print("Nhập tên: ");
+        String name1 = sc.nextLine();
+        person1.setName(name1);
+
+        System.out.print("Nhập tuổi: ");
+        int age1 = sc.nextInt();
+        person1.setAge(age1);
+
+        sc.nextLine();
+
+        // Nhập thông tin Person 2
+        System.out.println("\n=== NHẬP THÔNG TIN NGƯỜI THỨ HAI ===");
+
+        System.out.print("Nhập tên: ");
+        String name2 = sc.nextLine();
+        person2.setName(name2);
+
+        System.out.print("Nhập tuổi: ");
+        int age2 = sc.nextInt();
+        person2.setAge(age2);
+
+        // So sánh tuổi
+        System.out.println("\n=== KẾT QUẢ ===");
+
+        if (person1.getAge() > person2.getAge()) {
+            System.out.println(person1.getName() + " lớn tuổi hơn.");
+        } else if (person1.getAge() < person2.getAge()) {
+            System.out.println(person2.getName() + " lớn tuổi hơn.");
+        } else {
+            System.out.println("Hai người bằng tuổi nhau.");
+        }
+
+        sc.close();
+    }
+}
---
+++ Ex03/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+
+    // Thuộc tính
+    private String name;
+    private int age;
+
+    // Setter cho name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter cho name
+    public String getName() {
+        return name;
+    }
+
+    // Setter cho age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter cho age
+    public int getAge() {
+        return age;
+    }
+}
